d9f87f75c807aece4ef1f2d96b714ac165e505cf
mxmlnkn <mxmlnkn@github.de>
2023-08-12 21:06:30 +0200

    [API] Expose set_and_expand_lit_len_huffcode and make_inflate_huff_code_lit_len

--- igzip/igzip_inflate.c
+++ igzip/igzip_inflate.c
@@ -300,11 +300,11 @@ static int inline set_codes(struct huff_code *huff_code_table, int table_length,
 	return 0;
 }
 
-static int inline set_and_expand_lit_len_huffcode(struct huff_code *lit_len_huff,
-						  uint32_t table_length,
-						  uint16_t * count,
-						  uint16_t * expand_count,
-						  uint32_t * code_list)
+int set_and_expand_lit_len_huffcode(struct huff_code const * const lit_len_huff,
+						  uint32_t const table_length,
+						  uint16_t * const count,
+						  uint16_t * const expand_count,
+						  uint32_t * const code_list)
 {
 	int len_sym, len_size, extra_count, extra;
 	uint32_t count_total, count_tmp;
@@ -410,10 +410,10 @@ static int inline index_to_sym(int index)
 /* Sets result to the inflate_huff_code corresponding to the huffcode defined by
  * the lengths in huff_code_table,where count is a histogram of the appearance
  * of each code length */
-static void make_inflate_huff_code_lit_len(struct inflate_huff_code_large *result,
-					   struct huff_code *huff_code_table,
-					   uint32_t table_length, uint16_t * count_total,
-					   uint32_t * code_list, uint32_t multisym)
+void make_inflate_huff_code_lit_len(struct inflate_huff_code_large * const result,
+					   struct huff_code * const huff_code_table,
+					   uint32_t const /* table_length */, uint16_t const * const count_total,
+					   uint32_t * const code_list, uint32_t const multisym)
 {
 	int i, j;
 	uint16_t code = 0;
